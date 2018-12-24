9dc6039bd58a0ad0e02ad3f4b37d7f14d31ebc34
yuberalberto <yuberalberto@gmail.com>
2018-12-23 23:20:06 -0500

    clean up code

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Card.css';
 
 // export const Card = ({ id, name, email}) =>{
 const Card = ({ id, name, username, email}) =>{
@@ -9,7 +8,7 @@ const Card = ({ id, name, username, email}) =>{
         // <h1>RoboFriend</h1>
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
-            <div id='div2' className='w5 h4'>
+            <div id='div2' style={{padding:10, display:'flex', flexDirection:'column'}} className='w5 h4'>
                 <h2 className='f5 adjustxt'>{name}</h2>
                 <p>{email}</p>
             </div>
