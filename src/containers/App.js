498c6406453759bf1b7748b73300c59dc4c82729
yuberalberto <yuberalberto@gmail.com>
2018-12-26 21:52:14 -0500

    Finished the app and the React-Redux section

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,40 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
+import { setSearchField, requestRobots } from '../actions'
+
+const mpaStateToProps = state =>{
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
+}
 
 class App extends React.Component{ //Could use destructuring in React.Component
-  constructor(){
-    super();
-    this.state={
-      robots: [],
-      searchfield: ''
-    }
-  }
 
   componentDidMount(){
-    // console.log(this.props.store.getState());
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({robots: users}));
-  }
-
-  onSearchChange = (event) =>{
-    this.setState({searchfield: event.target.value});
+    this.props.onRequestRobots()
   }
 
   render(){
-    const {robots, searchfield} = this.state;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+      return robot.name.toLowerCase().includes(searchField.toLowerCase())
     });
-    return !robots.length ?
+    return isPending ?
     <h1>Loading...</h1>:(
       <div  className='tc'>
         <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        <SearchBox searchChange={onSearchChange}/>
         <Scroll>
           <ErrorBoundry>
             <CardList robots={filteredRobots}/>
@@ -45,4 +49,4 @@ class App extends React.Component{ //Could use destructuring in React.Component
   }
 }
 
-export default App;
+export default connect(mpaStateToProps, mapDispatchToProps)(App);
