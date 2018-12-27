d559cd0f8798624a07ef9ac812abe2ee374c43c5
yuberalberto <yuberalberto@gmail.com>
2018-12-27 00:27:16 -0500

    changed the img's alt name of the Card.js to firstname

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,18 +2,17 @@ import React from 'react';
 
 // export const Card = ({ id, name, email}) =>{
 const Card = ({ id, name, username, email}) =>{
-
-
+    let firstname=name.split(" ");
+    
     return (
-        // <h1>RoboFriend</h1>
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+            <img alt={firstname[0]} src={`https://robohash.org/${id}?size=200x200`} />
             <div id='div2' style={{padding:10, display:'flex', flexDirection:'column'}} className='w5 h4'>
                 <h2 className='f5 adjustxt'>{name}</h2>
                 <p>{email}</p>
             </div>
         </div>
-    );
+    )
 }
 
 export default Card;
