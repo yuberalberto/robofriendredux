892cf7ea6be8f9b60f8ae605893b362fa8e84fc3
yuberalberto <yuberalberto@gmail.com>
2019-01-26 18:32:45 -0500

    Updates

---
+++ precache-manifest.2d58c89706f7c2f9f4c905355d524f75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "18a3910b0058d9c34853",
+    "url": "/robofriendsredux/static/css/main.c382ef10.chunk.css"
+  },
+  {
+    "revision": "18a3910b0058d9c34853",
+    "url": "/robofriendsredux/static/js/main.18a3910b.chunk.js"
+  },
+  {
+    "revision": "52dcd770c596ae34eafc",
+    "url": "/robofriendsredux/static/css/1.6dd219d4.chunk.css"
+  },
+  {
+    "revision": "52dcd770c596ae34eafc",
+    "url": "/robofriendsredux/static/js/1.52dcd770.chunk.js"
+  },
+  {
+    "revision": "4d2456feee5a49224d96",
+    "url": "/robofriendsredux/static/js/runtime~main.4d2456fe.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendsredux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "4a563fda39ca6962dbfa4b857f74ac7f",
+    "url": "/robofriendsredux/index.html"
+  }
+];
